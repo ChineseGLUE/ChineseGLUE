c84e54236be6118d524775911c2b81359ff33903
faf <aa@qq.com>
2019-10-16 05:26:56 +0000

    test

--- baselines/models/albert/tpu/run_classifier_lcqmc_tiny.sh
+++ baselines/models/albert/tpu/run_classifier_lcqmc_tiny.sh
@@ -1,9 +1,9 @@
 CURRENT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 CURRENT_TIME=$(date "+%Y%m%d-%H%M%S")
-TASK_NAME="inews"
-export ALBERT_XLARGE_DIR=gs://models_zxw/prev_trained_models/nlp/albert-xlarge/albert_xlarge_zh_183k
+TASK_NAME="lcqmc"
+export ALBERT_XLARGE_DIR=gs://models_zxw/prev_trained_models/nlp/albert_tiny
 export DATA_DIR=gs://data_zxw/nlp/chineseGLUEdatasets.v0.0.1/$TASK_NAME
-export OUTPUT_DIR=gs://models_zxw/fine_tuning_models/nlp/albert-xlarge/albert_xlarge_zh_183k/tpu/$TASK_NAME/$CURRENT_TIME
+export OUTPUT_DIR=gs://models_zxw/fine_tuning_models/nlp/albert_tiny/tpu/${TASK_NAME}_$CURRENT_TIME
 
 python $CURRENT_DIR/../run_classifier.py \
   --task_name=$TASK_NAME \
@@ -11,11 +11,11 @@ python $CURRENT_DIR/../run_classifier.py \
   --do_eval=true \
   --data_dir=$DATA_DIR \
   --vocab_file=$ALBERT_XLARGE_DIR/vocab.txt \
-  --bert_config_file=$ALBERT_XLARGE_DIR/albert_config_xlarge.json \
+  --bert_config_file=$ALBERT_XLARGE_DIR/albert_config_tiny.json \
   --init_checkpoint=$ALBERT_XLARGE_DIR/albert_model.ckpt \
-  --max_seq_length=512 \
-  --train_batch_size=32 \
-  --learning_rate=2e-5 \
-  --num_train_epochs=8.0 \
+  --max_seq_length=128 \
+  --train_batch_size=64 \
+  --learning_rate=1e-4 \
+  --num_train_epochs=5.0 \
   --output_dir=$OUTPUT_DIR \
   --num_tpu_cores=8 --use_tpu=True --tpu_name=grpc://10.1.101.2:8470
